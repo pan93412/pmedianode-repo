45e20a597812320d22576bb51dfa9cf2af389e54
pan93412 <pan93412@gmail.com>
2019-07-23 16:12:18 +0800

    v1.0.0: PMedia -> pMedia

--- util/index.js
+++ util/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    brand: "PMediaNode",
+    brand: "pMediaNode",
     info: text => { console.info(`[I] ${text}`) },
     warn: text => { console.warn(`[W] ${text}`) },
     err: text => { console.error(`[E] ${text}`) }
