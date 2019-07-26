f4fc9b0dd765b41daee221a88c2f40f108b97a6c
pan93412 <pan93412@gmail.com>
2019-07-26 20:07:32 +0800

    v2.1.4: 新增英文語系 [EXPERIMENTAL]

---
+++ data/strings/en_US/logging.js
@@ -0,0 +1,20 @@
+/*
+ * Strings which is used on debug messages.
+ *
+ * CONTRIBUTORS:
+ *     pan93412 <pan93412@gmail.com>, 2019.
+ *
+ * LAST UPDATE:
+ *     2019/07/26
+ */
+
+module.exports = {
+  // %1$s: IP
+  about_browsed: '%s is looking over about page.',
+  announce_browsed: '%s is looking over announcements page.',
+  index_browsed: '%s went into homepage.',
+  player_watched: '%s is watching the video served on %s.', // %1$s: IP, %2$s: path
+  search_template: '%s %s', // %1$s: IP, %2$s: action
+  search_action_searchedNothing: 'went into the search page.',
+  search_action_searchedSomething: 'searched %s' // %1$s: query
+}
---
+++ data/strings/en_US/page.js
@@ -0,0 +1,35 @@
+/*
+ * Strings which is used on pages.
+ *
+ * CONTRIBUTORS:
+ *     pan93412 <pan93412@gmail.com>, 2019.
+ *
+ * LAST UPDATE:
+ *     2019/07/26
+ */
+
+module.exports = {
+  /* PUBLIC */
+  public_play: 'PLAY', // used on: index.pug, search.pug
+
+  /* NAVBAR */
+  navbar_home: 'Home',
+  navbar_announce: 'Announcements',
+  navbar_about: 'About',
+  navbar_play: 'Player',
+  navbar_search: 'Search',
+  navbar_admin: 'Manage',
+
+  /* TITLE */
+  title_searchPlaceHolder: 'Try "pan93412"?',
+  title_searchBtn: 'Search',
+
+  /* SEARCH */
+  search_result: 'Result of %s',
+
+  /* PLAYER */
+  player_error: 'ERROR!',
+  player_notExist: 'The media might not exists, or the ID you input is wrong!',
+  player_publisher: 'Publisher: ',
+  player_tags: 'Tags: '
+}
