07a03f9e3d83bd9f0231f5996431c38833ad8ff9
pan93412 <pan93412@gmail.com>
2019-07-26 19:31:38 +0800

    v2.1.4: 新增頁面字串

---
+++ data/strings/zh_TW/page.js
@@ -0,0 +1,35 @@
+/*
+ * Strings which is used on pages.
+ *
+ * CONTRIBUTORS:
+ *     pan93412 <pan93412@gmail.com>, 2019.
+ *
+ * LAST UPDATE:
+ *     2019/07/26
+ */
+
+module.exports = {
+  /* PUBLIC */
+  public_play: '播放', // used on: index.pug, search.pug
+
+  /* NAVBAR */
+  navbar_home: '首頁',
+  navbar_announce: '公告',
+  navbar_about: '關於',
+  navbar_play: '播放器',
+  navbar_search: '搜尋',
+  navbar_admin: '管理',
+
+  /* TITLE */
+  title_searchPlaceHolder: '試試看「pan93412」？',
+  title_searchBtn: '搜尋影片',
+
+  /* SEARCH */
+  search_result: '%s 的搜尋結果',
+
+  /* PLAYER */
+  player_error: '錯誤',
+  player_notExist: '媒體可能不存在，或您輸入的媒體 ID 錯誤！',
+  player_publisher: '發布者：',
+  player_tags: '標籤：'
+}
