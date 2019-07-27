bea07945fbd702de3dadee9f4f4e0a6a7238f4c5
pan93412 <pan93412@gmail.com>
2019-07-27 19:19:38 +0800

    v2.1.5: 獨立 gravatar 取得頭貼函式

--- utils/index.js
+++ utils/index.js
@@ -37,7 +37,19 @@ function tagsParser (taglist, isCode = false, delimiter = ', ') {
   return tagResult
 }
 
+/**
+ * 取得一位使用者的 Gravatar 頭貼。
+ *
+ * @param {string} 使用者 E-mail
+ * @return {string} 頭貼位址
+ */
+function getGravatar (email) {
+  const md5 = require('blueimp-md5') // 需要時才載入。
+  return 'https://www.gravatar.com/avatar/' + md5(email.toLowerCase()) + '?s=160'
+}
+
 module.exports = {
   getCurrentMediaData: getCurrentMediaData,
-  tagsParser: tagsParser
+  tagsParser: tagsParser,
+  getGravatar: getGravatar
 }
