d1bddd19c84c412c10cccdbc976f344ed09dc7a3
pan93412 <pan93412@gmail.com>
2019-07-27 20:21:26 +0800

    v2.1.5: 將統一函式庫套用至 player.js

--- routes/player.js
+++ routes/player.js
@@ -1,14 +1,14 @@
 const app = require('express')
-const log = require('../utils/log.js')
+const mods = require('./mods.js')
 const utils = require('../utils')
-const config = require('../data/config.js')
+const log = mods.log
+const config = mods.conf
 const fs = require('fs')
 const url = require('url')
 
 // l10n
-const sprintf = require('sprintf-js').sprintf
+const sprintf = mods.sprintf
 const loggingStr = require(`../data/strings/${config.lang}/logging.js`)
-const pageStr = require(`../data/strings/${config.lang}/page.js`)
 
 var playerRouter = app.Router()
 
@@ -19,7 +19,7 @@ const rawMediaData = fs.readFileSync('data/mediaList.json', {
 const mediaData = JSON.parse(rawMediaData.toString()).reverse()
 
 playerRouter.use(
-  (req, res, next) => {
+  (req, _, next) => {
     if (config.verbose) log.info(sprintf(loggingStr.player_watched, req.ip, req.url))
     next()
   }
@@ -27,16 +27,12 @@ playerRouter.use(
 
 playerRouter.get('/player/:id', (req, res) => {
   const vidData = utils.getCurrentMediaData(mediaData, req.params.id)
-  res.render('player', {
-    brand: config.brand,
-    userQuery: req.query.q,
+  res.render('player', Object.assign({
     vidData: vidData,
     vidID: req.params.id,
     vidTags: utils.tagsParser(vidData.tags, true),
-    cardWidth: config.cardWidth,
-    url: url, // URL Module
-    strings: pageStr
-  })
+    url: url // URL Module, which is used to fix m3u8 issues.
+  }, mods.stdRoutes))
 })
 
 module.exports = playerRouter
