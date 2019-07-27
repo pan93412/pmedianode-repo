24f76216d31aaea7c42f334a67dd16ace8046a97
pan93412 <pan93412@gmail.com>
2019-07-27 20:08:41 +0800

    v2.1.5: 將統一函式庫套用至 announce.js

--- routes/announce.js
+++ routes/announce.js
@@ -1,35 +1,32 @@
 const app = require('express')
-const log = require('../utils/log.js')
-const config = require('../data/config.js')
+const mods = require('./mods.js')
+const log = mods.log
+const config = mods.conf
 const fs = require('fs')
 var annoRouter = app.Router()
 
 // l10n
-const sprintf = require('sprintf-js').sprintf
+const sprintf = mods.sprintf
 const loggingStr = require(`../data/strings/${config.lang}/logging.js`)
-const pageStr = require(`../data/strings/${config.lang}/page.js`)
 
 const rawAnnoData = fs.readFileSync('data/announcements.json', {
   encoding: 'UTF-8'
 })
 
-const annoData = JSON.parse(rawAnnoData.toString()).reverse()
+const annoData = rawAnnoData.toJSON().reverse()
 
 annoRouter.use(
-  (req, res, next) => {
+  (req, _, next) => {
     if (config.verbose) log.info(sprintf(loggingStr.announce_browsed, req.ip))
     next()
   }
 )
 
 annoRouter.get('/announce', (req, res) => {
-  res.render('announce', {
-    brand: config.brand,
-    userQuery: req.query.q,
-    annoDat: annoData,
-    cardWidth: config.cardWidth,
-    strings: pageStr
-  })
+  res.render('announce', Object.assign({
+    annoDat: annoData
+  }, mods.stdRoutes)
+  )
 })
 
 module.exports = annoRouter
