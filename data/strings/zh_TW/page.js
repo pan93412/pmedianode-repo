9b65f62ca15f98a49d1b001c53a471d43f36e2c5
pan93412 <pan93412@gmail.com>
2019-07-27 21:33:46 +0800

    v2.1.6: 更新中文（繁體）字串檔

--- data/strings/zh_TW/page.js
+++ data/strings/zh_TW/page.js
@@ -5,7 +5,7 @@
  *     pan93412 <pan93412@gmail.com>, 2019.
  *
  * LAST UPDATE:
- *     2019/07/26
+ *     2019/07/27
  */
 
 module.exports = {
@@ -31,5 +31,15 @@ module.exports = {
   player_error: '錯誤',
   player_notExist: '媒體可能不存在，或您輸入的媒體 ID 錯誤！',
   player_publisher: '發布者：',
-  player_tags: '標籤：'
+  player_tags: '標籤：',
+
+  /* ABOUT */
+  about_app: '此站程式',
+  about_readme: '介紹及說明文件',
+  about_repo: 'GitHub 檔案庫',
+  // vvv Priority: low vvv
+  about_founder: '程式作者',
+  about_cofounder: '程式協作者',
+  about_contact: '聯絡方式',
+  about_email: '電子信箱'
 }
