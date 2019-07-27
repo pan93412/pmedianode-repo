ef89bc1853dabf4a08df927089b5e1fa9a7beb87
pan93412 <pan93412@gmail.com>
2019-07-27 19:38:24 +0800

    v2.1.5: 清理沒必要的依賴：about 頁面

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,6 @@
 const app = require('express')
 const log = require('../utils/log.js')
 const config = require('../data/config.js')
-const md5 = require('blueimp-md5')
 
 // l10n
 const sprintf = require('sprintf-js').sprintf
@@ -11,7 +10,7 @@ const pageStr = require(`../data/strings/${config.lang}/page.js`)
 var aboutRouter = app.Router()
 
 aboutRouter.use(
-  (req, res, next) => {
+  (req, _, next) => {
     if (config.verbose) log.info(sprintf(loggingStr.about_browsed, req.ip))
     next()
   }
@@ -21,7 +20,6 @@ aboutRouter.get('/about', (req, res) => {
   res.render('about', {
     brand: config.brand,
     userQuery: req.query.q,
-    md5: md5,
     showAuthors: config.showAuthors,
     strings: pageStr
   })
--- src/about.js
+++ src/about.js
@@ -1,3 +1,23 @@
 const brandLogo = require('./pMediaNodeLogo.svg')
+const conf = require('../data/config.js')
 
-document.querySelector('#brandLogo').setAttribute('src', '/assets/' + brandLogo)
+/**
+ * 將一個 ID 的 src 屬性設為指定值。
+ *
+ * @param {string} id, like '#brandLogo'
+ * @param {string} value
+ */
+function specifyValue(id, value) {
+  document.querySelector(id)
+          .setAttribute('src', value)
+}
+
+specifyValue('#brandLogo', '/assets/' + brandLogo)
+
+if (conf.showAuthors) {
+  const utils = require('../utils')
+  
+  specifyValue('#pan93412-logo', utils.getGravatar('pan93412@gmail.com'))
+  specifyValue('#hugwalk-logo', utils.getGravatar('baozou0421@gmail.com'))
+  specifyValue('#U2FsdGVKX1-logo', utils.getGravatar('U2FsdGVKX1@gmail.com'))
+}
