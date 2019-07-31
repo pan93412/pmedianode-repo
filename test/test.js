4e1ebf662c47acebe8eff54ebee58506c29bd206
pan93412 <pan93412@gmail.com>
2019-07-31 19:16:01 +0800

    v2.1.10: 已經完成 utils 的所有 Unit Test。

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 const utils = require('../utils')
-// const utilsWebpack = require('./utils/index.webpack.js')
+const utilsWebpack = require('../utils/index.webpack.js')
 
 // eslint-disable-next-line no-undef
 describe('檢查 utils - getCurrentMediaData', function () {
@@ -59,10 +59,10 @@ describe('檢查 utils - getCurrentMediaData', function () {
 
   // eslint-disable-next-line no-undef
   it('若請求不存在的 ID 時，是否回傳 undefined', function () {
-    assert(utils.getCurrentMediaData(
+    assert.ifError(utils.getCurrentMediaData(
       tests.full,
       'awwwwww'
-    ) == null)
+    ))
   })
 
   // eslint-disable-next-line no-undef
@@ -75,6 +75,57 @@ describe('檢查 utils - getCurrentMediaData', function () {
 })
 
 // eslint-disable-next-line no-undef
-describe('檢查 utils - taglist', function () {
+describe('檢查 utils - tagsParser', function () {
+  const test = ['hi', 'i', 'am', 'unit', 'test']
 
+  // eslint-disable-next-line no-undef
+  it('在 delimiter 與 isCode 都是預設值時，是否輸出正確？', function () {
+    assert.deepStrictEqual(utils.tagsParser(test), 'hi, i, am, unit, test')
+  })
+
+  // eslint-disable-next-line no-undef
+  it('在 delimiter 是預設值，isCode 是 true 時，是否輸出正確？', function () {
+    assert.deepStrictEqual(utils.tagsParser(test, true), '<code>hi, i, am, unit, test</code>')
+  })
+
+  // eslint-disable-next-line no-undef
+  it('在 isCode 是預設值，delimiter 是「、」時，是否輸出正確？', function () {
+    assert.deepStrictEqual(utils.tagsParser(test, false, '、'), 'hi、i、am、unit、test')
+  })
+
+  // eslint-disable-next-line no-undef
+  it('在 isCode 是 true，delimiter 是「、」時，是否輸出正確？', function () {
+    assert.deepStrictEqual(utils.tagsParser(test, true, '、'), '<code>hi、i、am、unit、test</code>')
+  })
+})
+
+// eslint-disable-next-line no-undef
+describe('檢查 utils - loadFile', function () {
+  // eslint-disable-next-line no-undef
+  it('在檔案不存在時，是否正確拋出錯誤？', function () {
+    try {
+      utils.loadFile('test/abcdefghijklmnopqrstuvwxyzwwwwwwwwwwwwwwww.abccccccccccc')
+    } catch (_) {
+      assert.ok(true)
+    }
+  })
+
+  // eslint-disable-next-line no-undef
+  it('在檔案存在時，是否正確輸出內容？', function () {
+    assert.deepStrictEqual(
+      utils.loadFile('test/testFile.txt').toString(),
+      'Hello! This is a test file. Don\'t move, delete or even modify it, otherwise Unit Test would be failed. '
+    )
+  })
+})
+
+// eslint-disable-next-line no-undef
+describe('檢查 utils - getGravatar', function () {
+  // eslint-disable-next-line no-undef
+  it('是否產生正確 Gravatar 頭貼位置', function () {
+    assert.deepStrictEqual(
+      utilsWebpack.getGravatar('pan93412@localhost'),
+      'https://www.gravatar.com/avatar/4bfb0c8cabc29a845112d63df1ce5290?s=160'
+    )
+  })
 })
