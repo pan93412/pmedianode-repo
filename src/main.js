eff3ceb4a772cf7250a302bd1823b3078fe7b324
pan93412 <pan93412@gmail.com>
2019-08-01 17:31:52 +0800

    v2.2.0: 完成嵌入圖示函數的撰寫作業

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 require('bootstrap') // Bootstrap JS
+const utils = require('../utils/index.webpack.js') // Utils
 const Cookie = require('js-cookie') // JS Cookie
 
 // 建立 click 活動
@@ -10,3 +11,15 @@ for (const langTags of document.querySelectorAll('.dropdown-menu > .changeLang')
     location.reload()
   })
 }
+
+/**
+ * 嵌入 iconData 圖示至 elementId
+ * @param {string} elementId 元素 ID
+ * @param {Object} iconData 圖示 ID
+ */
+function embedIcons (elementId, iconData) {
+  document.querySelector('#' + elementId)
+    .append(iconData.toSvg())
+}
+
+embedIcons()
--- utils/index.webpack.js
+++ utils/index.webpack.js
@@ -9,6 +9,21 @@ function getGravatar (email) {
   return 'https://www.gravatar.com/avatar/' + md5(email.toLowerCase()) + '?s=160'
 }
 
+/**
+ * 取得 GitHub Octicons 的某張圖示中介資訊。
+ *
+ * @param {string} id 圖示 ID。
+ * 請到 https://octicons.github.com/ 查圖示。
+ * @return {Object} 該 Octicons 圖示的中介資訊。
+ * 使用 toSvg() 嵌入網頁。
+ */
+function getOpticons (id) {
+  const oicon = require('octicons')
+
+  return oicon[id]
+}
+
 module.exports = {
-  getGravatar: getGravatar
+  getGravatar: getGravatar,
+  getOpticons: getOpticons
 }
