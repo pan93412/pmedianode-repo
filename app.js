4dfee574b9b6440da991c6068046f2e236d18631
pan93412 <pan93412@gmail.com>
2019-09-11 22:09:08 +0800

    feat(server): 後端伺服器現在僅專注於提供 API 及寄存頁面

--- app.js
+++ app.js
@@ -2,51 +2,17 @@ const express = require('express')
 var app = express()
 const routers = require('./routes')
 const config = require('./data/config.js')
-const log = require('./utils/log.js')
-const proc = require('process')
-
-app.use(require('cookie-parser')())
-
-// 檢查預設字串檔是否存在
-try {
-  require(`./data/strings/${config.lang}/page.js`)
-} catch (e) {
-  log.err("We can't load strings file: " + e.message)
-  proc.exit(1)
-}
-
-// 預設使用可愛的 PUG 引擎。
-app.set('view engine', 'pug')
-
-// assets 的存放處
-app.use('/assets', express.static('assets'))
 
 // 使用者可自訂的 data 存放處
-app.use('/assets/data', express.static('data'))
+app.use('/data', express.static('data'))
 
-/*
- * Routers
- */
+// API
+app.use('/api', routers.api)
 
-// Home 頁面
-app.get('/', routers.index)
+// pMediaNode 的 UI 位置
+app.use('/', express.static('dist'))
 
-// About 頁面
-app.get('/about', routers.about)
-
-// Announce 頁面（需在 config.js 中啟用公告功能）
-if (config.enableAnnounce) app.get('/announce', routers.announce)
-
-// Search 頁面
-app.get('/search', routers.search)
-
-// Player 頁面
-app.get(/\/player\/.+/, routers.player)
-
-// Options 頁面
-app.get('/options', routers.options)
-
-// 然後，搞個伺服器
+// 設定伺服器
 if (config.isHttps) {
   const https = require('https')
   const server = https.createServer(config.httpsOptions, app)
