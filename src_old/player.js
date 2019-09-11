410dc7c7cc1d7e7719b30d1cb858c98e25beace5
pan93412 <pan93412@gmail.com>
2019-09-11 23:00:31 +0800

    feat: 移除所有過時元件
    BREAKING CHANGES: 進入開發狀態 - 主要元件可能無法使用

--- src_old/player.js
+++
@@ -1,3 +0,0 @@
-require('video.js') // Video.js JS
-require('video.js/dist/video-js.css') // Video.js CSS
-require('@videojs/http-streaming') // Video.js for HLS
--- utils/log.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  info: text => { console.info(`[I] ${text}`) },
-  warn: text => { console.warn(`[W] ${text}`) },
-  err: text => { console.error(`[E] ${text}`) }
-}
